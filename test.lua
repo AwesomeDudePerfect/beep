37fecd20f60e25c87ebffc60452ce3d31f93aada
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2023-12-26 18:46:59 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -187,8 +187,8 @@ local function teleport(x, y, z)
 end
 
 create_platform(-922, 190, -2338)
-wait(1)
-teleport(-922, 190, -2338)
+wait(10)
+teleport(-922, 195, -2338)
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
     local playerID = message['PlayerID']

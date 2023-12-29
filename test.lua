ab4d229a807ad235f9f0779c909357568aac27c6
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2023-12-29 21:05:28 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -9,6 +9,8 @@ wait(20)
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
+local lighting = game.Lighting
+local terrain = game.Workspace.Terrain
 local serverStats = game:GetService("Stats").Network.ServerStatsItem
 local dataPing = serverStats["Data Ping"]:GetValueString()
 local pingValue = tonumber(dataPing:match("(%d+)"))
@@ -324,7 +326,38 @@ end)
 game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Disabled = true
 
 --//OPTIMIZATION
-game:GetService('RunService'):Set3dRenderingEnabled(true)
+terrain.WaterWaveSize = 0
+terrain.WaterWaveSpeed = 0
+terrain.WaterReflectance = 0
+terrain.WaterTransparency = 0
+lighting.GlobalShadows = false
+lighting.FogStart = 0
+lighting.FogEnd = 0
+lighting.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
+for i, v in pairs(game:GetDescendants()) do
+    if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    elseif v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    end
+end
+
+for i, e in pairs(lighting:GetChildren()) do
+    if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+        e.Enabled = false
+    end
+end
+game:GetService('RunService'):Set3dRenderingEnabled(false)
 
 --//DEAD SERVER CHECK
 local isServerDead = coroutine.create(function ()

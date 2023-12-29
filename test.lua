6240925b86bd94ab2f9ae26aea9ef12d38470f42
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2023-12-30 03:05:40 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,8 @@
 --//WAIT UNTIL GAME LOADS PROPERLY
 repeat wait() until game:IsLoaded()
-if not game.PlaceId == 8737899170 or not game.PlaceId == 15502339080 or not game.PlaceId == 15588442388 then wait(9e9) end
+if game.PlaceId ~= 8737899170 or game.PlaceId ~= 15502339080 or game.PlaceId ~= 15588442388 then
+    wait(9e9)
+end
 print('executed')
 
 --//VARIABLES
